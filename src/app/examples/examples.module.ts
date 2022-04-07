88e68f133759922b8efec4b9c07a31408d274f66
BacemBenDaly <bendalybacem@gmail.com>
2022-04-07 14:46:28 +0100

    Fixed routing 🔵

--- src/app/examples/examples.module.ts
+++ src/app/examples/examples.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { LandingComponent } from './landing/landing.component';
+import { LandingComponent } from '../components/landing/landing.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SignupComponent } from './signup/signup.component';
 
