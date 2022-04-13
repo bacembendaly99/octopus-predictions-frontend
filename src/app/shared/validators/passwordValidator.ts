4051deb3c5e069a33552e3f67c8223c0a746819d
BacemBenDaly <bendalybacem@gmail.com>
2022-04-13 11:03:40 +0100

    Validator

--- src/app/shared/validators/passwordValidator.ts
+++ src/app/shared/validators/passwordValidator.ts
@@ -4,8 +4,8 @@ export class PasswordValidator {
     static passwordsMatch(control: AbstractControl): ValidationErrors {
         const password = control.get('password').value;
         const repeatPassword = control.get('repeatPassword').value;
-        if ((password === repeatPassword) && password != null && repeatPassword !== null) return null;
-        else
+        if ((password === repeatPassword) && password != null && repeatPassword !== null) { return null; } else {
             return {passwordsNotMatching: true};
+        }
     }
 }
